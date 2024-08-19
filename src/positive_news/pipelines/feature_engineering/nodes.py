47786effa36e1e270627fcbe49370e687cb41dc3
BWallyn <ben96.wallyn@gmail.com>
2024-08-19 11:03:07 +0200

    function to prepare dataframe for hugging face

--- src/positive_news/pipelines/feature_engineering/nodes.py
+++ src/positive_news/pipelines/feature_engineering/nodes.py
@@ -2,3 +2,29 @@
 This is a boilerplate pipeline 'feature_engineering'
 generated using Kedro 0.19.7
 """
+# =================
+# ==== IMPORTS ====
+# =================
+
+import pandas as pd
+
+# ===================
+# ==== FUNCTIONS ====
+# ===================
+
+def prepare_data_for_hf(df: pd.DataFrame) -> pd.DataFrame:
+    """Prepare dataframe for HuggingFace datasets
+
+    Args:
+        df (pd.DataFrame): Input dataframe
+    Returns:
+        (pd.DataFrame): Output dataframe with just the needed columns
+    """
+    df = df.rename(columns={
+        "title": "text",
+        "sentiment": "label"
+    })
+    if "label" in df.columns:
+        return df[["text", "label"]]
+    else:
+        return df[["text"]]
